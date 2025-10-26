b423c40162d33c5e1b11a2e236704580d6567071
Adrish-BITS <2023tm93518@wilp.bits-pilani.ac.in>
2025-10-26 22:00:57 +0530

    Adding main code

---
+++ src/main/java/com/extickets/emailService/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.extickets.emailService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.extickets.emailService.model.EmailRequest;
+import com.extickets.emailService.service.EmailService;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+	
+	@PostMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+        return ResponseEntity.ok("Email sent successfully!");
+    }
+}
